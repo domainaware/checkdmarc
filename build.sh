5157656cbcc760a1d3273448b142f656caa991fa
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2022-09-08 08:50:23 -0400

    Fix build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 . venv/bin/activate
-pip3 install -U -r requirements.txt && cd docs && make html && cp -r build/html/* ../../checkdmarc-docs/ && cd .. && flake8 checkdmarc.py && flake8 tests.py && python3 tests.py && rm -rf dist/ build/ && hatch build
+pip3 install -U -r requirements.txt && cd docs && make html && cp -r build/html/* ../../checkdmarc-docs/ && cd .. && flake8 checkdmarc && flake8 tests.py && python3 tests.py && rm -rf dist/ build/ && hatch build
 
 
