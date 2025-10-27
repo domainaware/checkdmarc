4d4cc52949b36d007a40fe19264c030ec10e929c
Sean Whalen <seanthegeek@users.noreply.github.com>
2025-10-27 15:44:05 -0400

    5.12.16
    - Fix crash on DNS retry

--- checkdmarc/utils.py
+++ checkdmarc/utils.py
@@ -180,7 +180,7 @@ def query_dns(
             return query_dns(
                 domain,
                 record_type,
-                nameservers,
+                nameservers=nameservers,
                 resolver=resolver,
                 timeout=timeout,
                 timeout_retries=timeout_retries,
